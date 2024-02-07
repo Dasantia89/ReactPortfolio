f26bbf120e12094641e886e0c5f6ce5a5c4c8f17
Dasantia89 <143134358+Dasantia89@users.noreply.github.com>
2024-02-07 01:39:38 -0500

    modified image location

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ export default function HomePage() {
   return (
     <figure>
 
-        <img src="./assets/images/pic.png" alt="Picture of David Santiago"></img>
+        <img src="/assets/images/pic.png" alt="Picture of David Santiago"></img>
         <p>Aspiring Web Developer</p>
 
 
